2a9093f0c4e5906c644da432d715ceb71f49d7ac
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-09-08 15:43:29 +0530

    fixed errors and started working on reactjs

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -1,13 +1,25 @@
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tweets, setTweets] = useState([])
+  useEffect(() => {
+    // do my lookup
+    const tweetItems = [{"content": 123}, {"content": "Hello World"}]
+    setTweets(tweetItems)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
