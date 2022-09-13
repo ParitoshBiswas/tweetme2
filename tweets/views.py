5582aa941adaee85e8b0be42661acd56aaf8fed0
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-09-13 18:40:04 +0530

    API Methods in React

--- tweets/views.py
+++ tweets/views.py
@@ -32,7 +32,7 @@ def home_view(request, *args, **kwargs):
 # @authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetCreateSerializer(data = request.POST)
+    serializer = TweetCreateSerializer(data = request.data)
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
         return Response(serializer.data, status=201)
