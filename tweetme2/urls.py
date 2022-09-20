bd693e365c0f826f55f60f80a19a57ed5c9b24f6
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-09-20 16:25:03 +0530

    Signals to Create Profile Objects

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -24,7 +24,6 @@ from accounts.views import ( login_view, logout_view, register_view )
 from tweets.views import(
     tweets_list_view,
     tweets_detail_view,
-    tweets_profile_view,
 )
 
 urlpatterns = [
@@ -34,7 +33,7 @@ urlpatterns = [
     path('logout/', logout_view),
     path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
-    path('profile/<str:username>', tweets_profile_view),
+    re_path(r'profiles?/', include('profiles.urls')),
     path('api/tweets/', include('tweets.api.urls'))
 ]
 
