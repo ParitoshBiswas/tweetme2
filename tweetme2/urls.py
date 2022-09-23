d8fde416ea4835a03ba5625d36878e735bed43a9
Paritosh Biswas <53013257+ParitoshBiswas@users.noreply.github.com>
2022-09-23 12:44:18 +0530

    User Profile API Detail

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -22,13 +22,15 @@ from django.views.generic import TemplateView
 from accounts.views import ( login_view, logout_view, register_view )
 
 from tweets.views import(
+    home_view,
     tweets_list_view,
     tweets_detail_view,
 )
 
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
-    path('', tweets_list_view),
+    path('global/', tweets_list_view),
     path('login/', login_view),
     path('logout/', logout_view),
     path('register/', register_view),
